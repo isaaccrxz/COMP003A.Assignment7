9ca184c686af6cc3a5f880e7d815bf96bf959892
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-03-05 23:20:36 -0800

    Add project files.

---
+++ COMP003A.Assignment7/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Author: Isaac Cruz
+ * Course: COMP003A
+ * Purpose: Basic Data Structors
+ */
+namespace COMP003A.Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a single letter.");
+            Console.ReadLine();
+            
+
+            Console.WriteLine("Insert a single word.");
+            Console.ReadLine();
+
+            List<string> names = new List<string>();
+            char userInput = default;
+
+            do
+            {
+                Console.Write("Enter a name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Press any key to add more or (e) to exit.");
+                Console.ReadLine(); 
+                int userinput;
+            }
+            while (false);
+        }
+
+        void SectionSeparator()
+        {
+            string parameter;
+        }
+
+        void CharacterCounter(char characterInput, string word)
+        {
+            int count = 0;
+            count += word.Length;
+            characterInput = char.ToUpper(characterInput);
+            word = word.ToLower();
+
+        }
+
+        void IsPalindrom(string word)
+        {
+            bool itWorked = true;
+            itWorked = false;
+            // come back to this
+        }
+
+        void TraverseList(List<string> list)
+        {
+            Console.WriteLine(list.Count);
+        }
+
+
+    }
+}
